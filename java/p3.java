a81da4cb405eec3713fc77dea354732996d2617f
isatyamks <isatyamks@gmail.com>
2025-03-06 00:08:37 +0530

    Add Java and Python programs for finding maximum numbers and printing greetings

---
+++ java/p3.java
@@ -0,0 +1,11 @@
+public class MaxNumber {
+    public static void main(String[] args) {
+        int x = 5, y = 10;
+        int max;
+        if (x > y);
+            max = x; 
+        else
+            max = y;
+        System.out.println("Max: " + max);
+    }
+}
---
+++ python/p3.py
@@ -0,0 +1,6 @@
+def find_largest(a, b):
+if a > b:
+    return a
+else:
+    return b
+print(find_largest(5, 10))  
