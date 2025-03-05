c20f8aef1ad55dd6ce40717ebcb58d2ea7f8f728
isatyamks <isatyamks@gmail.com>
2025-03-06 00:08:07 +0530

    Add initial C++ programs for basic input/output and control structures

---
+++ c++/p3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5, y = 10;
+    int max;
+    if (x > y);
+        max = x;  
+    else
+        max = y;
+    cout << "Max: " << max;
+    return 0;
+}
